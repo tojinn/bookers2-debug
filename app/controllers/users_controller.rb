4c598f2859d7ceb7d1b67681f9d15dc12f444dde
EC2 Default User <ec2-user@ip-172-31-26-232.ec2.internal>
2021-12-06 14:05:22 +0000

    [Update]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,6 +58,15 @@ class UsersController < ApplicationController
   
   def search
     @results = @q.result
+    @user = User.find(params[:user_id])
+    @books = @user.books 
+    @book = Book.new
+    if params[:created_at] == ""
+      @search_book = "日付を選択してください"
+    else
+      create_at = params[:created_at]
+      @search_book = @books.where(['created_at LIKE ? ', "#{create_at}%"]).count
+    end
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :users,only: [:show,:index,:edit,:update] do
+      get  "search", to: "users#search"
     member do
      get :follows, :followers
   end
